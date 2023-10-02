9cb8eedd250dad1d9d709655877b4edbaa564b65
Riya Tyagi <riyatyagi284@gmail.com>
2023-10-02 13:09:59 +0530

    few changes

---
+++ src/Constant.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        id:"our Story",
+        title:"OUR STORY"
+    },
+    {
+        id:"master blender",
+        title:"Master Blender",
+    },
+    {
+        id:"recipes",
+        title:"Recipes",
+    },
+    {
+        id:"instagram",
+        title:"INSTAGRAM",
+    }
+]
